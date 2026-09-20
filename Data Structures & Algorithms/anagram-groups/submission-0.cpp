4923e13834e871b3d981143a06d4fc5b78794d14
Disha Bhardwaj <67470541+bhardwajdisha@users.noreply.github.com>
2026-09-20 13:55:36 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;
+        unordered_map<string, vector<string>> mp;
+        for(string s:strs){
+            string temp = s;
+            sort(temp.begin(),temp.end());
+            mp[temp].push_back(s);
+        }
+        for(auto& pair: mp){
+            res.push_back(pair.second);
+        }
+        return res;
+    }
+};
